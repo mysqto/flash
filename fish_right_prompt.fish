3def5bcce7bb4d3859dbaee2a212c530180d0877
Chen Lei <my@mysq.to>
2019-05-14 23:20:51 +0800

    Update: better time display

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -106,7 +106,7 @@ function fish_right_prompt
     end)(flash_off)
   end
 
-  printf " "(flash_trd)(date +%H(status::color):(flash_dim)%M(status::color):(flash_trd)%S)(flash_snd)" "(flash_off)
+  printf " "(flash_trd)(date +%H(status::color):(flash_trd)%M(status::color):(flash_trd)%S)(flash_snd)" "(flash_off)
 
   if test $code -ne 0
     echo (flash_fst)"≡ "(flash_snd)"$code"(flash_off)
